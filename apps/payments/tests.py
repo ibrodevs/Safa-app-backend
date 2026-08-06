184ebcfd284d92f7c8f9ff1f7ebfff96a058d504
Ibrokhim <132742784+ibrodevs@users.noreply.github.com>
2026-08-06 17:28:46 +0600

    Test cascading payment attempt deletion

--- apps/payments/tests.py
+++ apps/payments/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.db import models
+from django.test import SimpleTestCase
 
-# Create your tests here.
+from apps.payments.models import PaymentAttempt
+
+
+class PaymentAttemptDeletionPolicyTests(SimpleTestCase):
+    def test_payment_attempts_are_deleted_with_shipment(self):
+        shipment_field = PaymentAttempt._meta.get_field("shipment")
+
+        self.assertIs(shipment_field.remote_field.on_delete, models.CASCADE)
