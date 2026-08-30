565f1903ae8aaa96db58e22f61b4d3c67989e6f4
ibrodevs <ibroabdraimov20@gmail.com>
2026-08-30 13:45:00 +0600

    commit

--- core/settings.py
+++ core/settings.py
@@ -346,3 +346,40 @@ STATIC_OTP_CODE = (
 ALLOW_STATIC_OTP_IN_PRODUCTION = os.getenv(
     "ALLOW_STATIC_OTP_IN_PRODUCTION", "0"
 ).strip().lower() in {"1", "true", "yes", "on"}
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "standard": {
+            "format": "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "INFO",
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": False,
+        },
+        "django.request": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+        "apps": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": False,
+        },
+    },
+}
